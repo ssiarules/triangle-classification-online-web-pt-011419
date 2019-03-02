c7b9c1a0452736e518843a7162b995cd9809a1a5
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-02 07:39:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  def kind
+     if  (side1 * side2 * side3) == 0 || (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 == side3) <= side3
+     
+      begin 
+        raise TriangleError 
+        puts error.message
+      end 
+     
+
+     elsif side1 == side2 && side1 == side3
+      return :equilateral 
+      
+      
+     elsif side1 == side2 || side1 == side3 || side2 == side3 
+      return :isosceles 
+      
+    else
+      return :scalene 
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    def message 
+      puts "not a triangle"
+    end 
+  end
+end 
+
+ 
